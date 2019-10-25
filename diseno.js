de372e14e938e8983ba8e81b0422a5e88f7ad46a
marcelo <marcelomartinez2.5.99@gmail.com>
2019-10-25 14:21:56 -0500

    add

--- diseno.js
+++ diseno.js
@@ -64,7 +64,11 @@ insert = message => {
   // INSERT THE POST OBJETO INTO THE DATABASE
 
   let query = connection.query(
-    `insert into designdatabase(latitude,longitude,time,hour,speed,car_id)  values (${message.lat},${message.long},${message.date}, ${message.hour}, ${message.speed}, ${message.carId});
+    `insert into designdatabase(latitude,longitude,time,hour,speed,car_id)  values (${
+      message.lat
+    },${
+      message.long
+    },${message.date.toString()}, ${message.hour.toString()}, ${message.speed.toString()}, ${message.carId.toString()});
   `,
     function(error, results, fields) {
       if (error) throw error;
